f272383477d16598f6550aa5c02ca8aef093b162
郑高红 <gaohong.zheng@workerbrother.com>
2018-08-27 09:59:56 +0800

    element样式修改

--- clota/src/config/index.js
+++ clota/src/config/index.js
@@ -5,6 +5,6 @@
 const dev = process.env.NODE_ENV === 'development';
 
 export default {
-    HOST: dev ? 'http://172.25.50.189:8888/' : '' //开发环境
+    HOST: dev ? 'http://172.25.50.189:8888/' : 'http://172.25.50.189:8888/' //开发环境
     // HOST : dev ? 'http://172.25.50.52:9080' : '' //李瑜
 }
