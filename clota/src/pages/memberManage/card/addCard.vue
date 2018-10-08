acd3fbfb4d5c3f9fb4df85e92334a06c8a775d94
yangzetao <624524240@qq.com>
2018-10-08 11:11:03 +0800

    会员卡卷 可用渠道修改 只显示自营渠道

--- clota/src/pages/memberManage/card/addCard.vue
+++ clota/src/pages/memberManage/card/addCard.vue
@@ -942,9 +942,12 @@
              * 查询渠道
              */
             queryChannelSet () {
-                ajax.post('querySelfChannel').then(res => {
+                ajax.post('querySelfOwnedChannel', {
+                    pageNo: 1,
+                    pageSize: 999
+                }).then(res => {
                     if(res.success){
-                        this.channelSetList = res.data ? res.data : [];
+                        this.channelSetList = res.data.data ? res.data.data : [];
                     }else{
                         this.channelSetList =  [];
                     }
