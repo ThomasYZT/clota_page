6ab69c153f175dae1d0a6c6d2d66f10c72ab7ad6
郑高红 <gaohong.zheng@workerbrother.com>
2018-10-12 17:53:52 +0800

    地址长度问题解决

---
+++ clota/src/pages/orderCenter/createOrder/orderWrite/index.vue
@@ -0,0 +1,42 @@
+<!--订单填写-->
+
+<template>
+    <div class="order-write">
+
+        <bread-crumb-head
+            :locale-router="'订单预定'"
+            :before-router-list="beforeRouterList">     <!--新增卡券 : 修改卡券信息-->
+        </bread-crumb-head>
+    </div>
+</template>
+
+<script>
+    import breadCrumbHead from '@/components/breadCrumbHead/index.vue';
+    export default {
+        components : {
+            breadCrumbHead
+        },
+        data() {
+            return {
+                //上级路由列表
+                beforeRouterList: [
+                    {
+                        name: '订单填写',
+                        router: {
+                            name: 'createOrder'
+                        }
+                    }
+                ],
+            }
+        },
+        methods: {}
+    }
+</script>
+
+<style lang="scss" scoped>
+	@import '~@/assets/scss/base';
+    .order-write{
+        @include block_outline();
+        background: $color_fff;
+    }
+</style>
