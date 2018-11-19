1c87ead5fc47309c533f344cb07e9b860ee848ce
郑高红 <gaohong.zheng@workerbrother.com>
2018-11-19 15:20:29 +0800

    菜单权限配置

--- clota/src/store/index.js
+++ clota/src/store/index.js
@@ -393,8 +393,8 @@ export default new Vuex.Store({
          * 初始化读卡器信息
          */
         initCardRead (store) {
-            // store.commit('updateCardReadEnabled',true);
-            // return;
+            store.commit('updateCardReadEnabled',true);
+            return;
             //如果window下没有rd这个对象，表示当前浏览器不支持activeX插件，或者没有启用activeX插件，
             if (window.rd ) {
                 try {
@@ -415,8 +415,8 @@ export default new Vuex.Store({
          */
         getCardReadData (store) {
             return new Promise((resolve,reject) => {
-                // store.commit('updateCardReadEnabled',true);
-                // resolve('555555');
+                store.commit('updateCardReadEnabled',true);
+                resolve('777777');
                 let st;
                 //如果window下没有rd这个对象，表示当前浏览器不支持activeX插件，或者没有启用activeX插件，
                 if (window.rd) {
