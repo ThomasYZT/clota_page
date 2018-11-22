18ef881e6aedba3dd5ab64201e075c6f015ffcc5
yangzetao <624524240@qq.com>
2018-11-22 16:17:02 +0800

    翻译

--- clota/src/assets/lang/modules/zhCN.js
+++ clota/src/assets/lang/modules/zhCN.js
@@ -1761,4 +1761,8 @@ export default {
     'score_add' : '积分发放',
     'score_reduce' : '积分消耗',
     'exchange_goods_reduce' : '积分兑换商品',
+
+    'unconfiguredAccount' : '你还未配置在线收款账户请联系客服',
+    'collectionAccountInformation' : '收款账户信息',
+    'collectionAccountType' : '收款账户类型'
 }
