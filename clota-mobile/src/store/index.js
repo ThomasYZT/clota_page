30dea0c26fb88908ef84daa535a2895ba4ba6010
yangzetao <624524240@qq.com>
2018-11-25 20:12:28 +0800

    store修改

--- clota-mobile/src/store/index.js
+++ clota-mobile/src/store/index.js
@@ -211,7 +211,7 @@ export default new Vuex.Store({
                         if (memberCardList.length > 0) {
                             //存储卡列表数据
                             commit('updateCardInfoList', memberCardList);
-                            commit('updateCardInfo', memberCardList[0]);
+                            commit('updateCardInfo');
                             resolve();
                         } else {
                             //会员卡列表数据为空
