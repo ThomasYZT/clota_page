11d7eeece97175e6811c499f37104a437eb9ace4
jingcheng.duan <jingcheng.duan@workbrother.com>
2018-11-29 17:45:13 +0800

    销售用户信息   暂存

--- clota/src/pages/universalMarketing/salesUserInfo/components/infoDetailModal.vue
+++ clota/src/pages/universalMarketing/salesUserInfo/components/infoDetailModal.vue
@@ -1,13 +1,179 @@
 <!--
-内容：
+内容：销售用户详细信息
 作者：djc
 日期：
 -->
 
 <template>
-    <div class="">
+    <Modal
+        v-model="visible"
+        :title="$t('销售用户详细信息')"
+        class-name="vertical-center-modal"
+        width="1000"
+        :mask-closable="false"
+        @on-cancel="hide">
 
-    </div>
+        <div class="modal-body">
+            <!--个人基本信息-->
+            <div class="info-item">
+                <div class="info-item-title">
+                    <span class="title-name">{{$t('个人基本信息')}}</span>
+                </div>
+                <div class="content">
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('name')}}：</span>
+                                <span class="info-val" v-w-title="'刘木子'">{{'刘木子' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('gender')}}：</span>
+                                <span class="info-val">{{$t('female') | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--身份证号码-->
+                                <span class="info-key">{{$t('身份证号码')}}：</span>
+                                <span class="info-val">{{'431129199411090989'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('birthday')}}：</span>
+                                <span class="info-val">{{'11-09'}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('mobilePhone')}}：</span>
+                                <span class="info-val">{{'176387263872'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                </div>
+            </div>
+            <!--收款账户信息-->
+            <div class="info-item">
+                <div class="info-item-title">
+                    <span class="title-name">{{$t('收款账户信息')}}</span>
+                </div>
+                <div class="content">
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--收款账户类型-->
+                                <span class="info-key">{{$t('collectionAccountType')}}：</span>
+                                <span class="info-val">{{$t('支付宝') | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--收款用户姓名-->
+                                <span class="info-key">{{$t('收款用户姓名')}}：</span>
+                                <span class="info-val">{{$t('张依依') | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('支付宝账号')}}：</span>
+                                <span class="info-val">{{'59789792839809'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                </div>
+            </div>
+            <!--注册信息-->
+            <div class="info-item">
+                <div class="info-item-title">
+                    <span class="title-name">{{$t('注册信息')}}</span>
+                </div>
+                <div class="content">
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--营销类别-->
+                                <span class="info-key">{{$t('marketType')}}：</span>
+                                <span class="info-val" v-w-title="'物美超市'">{{'物美超市' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--营销等级-->
+                                <span class="info-key">{{$t('marketingLevel')}}：</span>
+                                <span class="info-val" v-w-title="'物美超市1级'">{{'物美超市1级' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <span class="info-key">{{$t('signTime')}}：</span>
+                                <span class="info-val">{{'2018-08-06'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--用户邀请码-->
+                                <span class="info-key">{{$t('用户邀请码')}}：</span>
+                                <span class="info-val">{{'1109'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                </div>
+            </div>
+            <!--销售信息-->
+            <div class="info-item">
+                <div class="info-item-title">
+                    <span class="title-name">{{$t('销售信息')}}</span>
+                </div>
+                <div class="content" style="margin-bottom: 26px;">
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--累计销售额-->
+                                <span class="info-key">{{$t('totalMoney')}}：</span>
+                                <span class="info-val text-org">{{'3478.00'}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                    <i-row>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--佣金总额-->
+                                <span class="info-key">{{$t('佣金总额')}}：</span>
+                                <span class="info-val text-org">{{'8932.93' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--已提现金额-->
+                                <span class="info-key">{{$t('已提现金额')}}：</span>
+                                <span class="info-val text-org">{{'8932.93' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                        <i-col span="8">
+                            <div class="node-info">
+                                <!--未提现金额-->
+                                <span class="info-key">{{$t('未提现金额')}}：</span>
+                                <span class="info-val text-org">{{'8932.93' | contentFilter}}</span>
+                            </div>
+                        </i-col>
+                    </i-row>
+                </div>
+            </div>
+        </div>
+
+        <div slot="footer" class="modal-footer">
+            <Button type="ghost" style="width: 88px;"
+                    @click="hide" >{{$t("cancel")}}</Button>
+        </div>
+
+    </Modal>
 </template>
 <script>
 
@@ -15,7 +181,9 @@
         components : {},
         props : {},
         data () {
-            return {}
+            return {
+                visible : false,
+            }
         },
         computed : {},
         created () {
@@ -23,10 +191,88 @@
         mounted () {
         },
         watch : {},
-        methods : {}
+        methods : {
+            /**
+             * 弹窗弹出时传入销售用户详细信息数据
+             * @param data
+             */
+            show ( data ) {
+                if (data) {
+//                    this.salesUserInfo = data;
+
+                }
+
+                this.visible = true;
+            },
+            /**
+             * 关闭模态框
+             */
+            hide () {
+                this.visible = false;
+            },
+        }
     };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    @import '~@/assets/scss/base';
 
+    .modal-body {
+        margin: 14px 24px;
+        font-size: 14px;
+    }
+
+    .text-org {
+        color: $color_yellow !important;
+    }
+
+    .info-item {
+        .info-item-title {
+            position: relative;
+            height: 20px;
+            line-height: 20px;
+            &:after {
+                content: '';
+                display: inline-block;
+                border-top: 1px dashed #E1E1E1;
+                width: 100%;
+                vertical-align: middle;
+            }
+        }
+
+        .title-name {
+            position: absolute;
+            padding-right: 16px;
+            background: #fff;
+            z-index: 10;
+            font-size: 16px;
+            color: #354052;
+            letter-spacing: 1.14px;
+        }
+
+        .content {
+            margin: 20px 0 40px 0;
+        }
+
+        .node-info{
+            display: flex;
+            flex-direction: row;
+            height: 30px;
+            line-height: 30px;
+            @include overflow_tip();
+
+            .info-val{
+                flex: 1;
+                display: inline-block;
+                @include overflow_tip();
+                color: $color_666;
+                padding-right: 10px;
+            }
+            .info-key{
+                color: $color_333;
+                display: inline-block;
+                /*float: left;*/
+            }
+        }
+    }
 </style>
--- clota/src/pages/universalMarketing/salesUserInfo/salesUserInfo.vue
+++ clota/src/pages/universalMarketing/salesUserInfo/salesUserInfo.vue
@@ -1,21 +1,64 @@
 <!--
-内容：
+内容：销售用户信息
 作者：djc
 日期：
 -->
 
 <template>
-    <div class="">
-        销售用户信息
+    <div class="sales-user-info">
+        <!--列表数据筛选器-->
+        <sales-user-filter @on-search=""></sales-user-filter>
+        <!--销售用户信息列表-->
+        <table-com
+            :show-pagination="true"
+            :column-data="columnData"
+            :table-data="tableData"
+            :total-count="totalCount"
+            :border="true"
+            @query-data="queryList">
+
+            <el-table-column
+                slot="column9"
+                slot-scope="row"
+                :label="row.title"
+                fixed="right"
+                :width="row.width"
+                :min-width="row.minWidth">
+                <template slot-scope="scope">
+                    <ul class="operate-list">
+                        <li @click="showModal(scope.row)">{{$t('check')}}</li><!--查看-->
+                    </ul>
+                </template>
+            </el-table-column>
+        </table-com>
+
+        <!--销售用户详细信息 - 弹窗-->
+        <info-detail-modal ref="infoDetailModal"></info-detail-modal>
     </div>
 </template>
 <script>
+    import salesUserFilter from './components/salesUserFilter.vue';
+    import infoDetailModal from './components/infoDetailModal.vue';
+    import tableCom from '@/components/tableCom/tableCom.vue';
+    import ajax from '@/api/index';
+    import { salesUserHead } from './salesUserConfig';
 
     export default {
-        components : {},
+        components : {
+            salesUserFilter,
+            tableCom,
+            infoDetailModal
+        },
         props : {},
         data () {
-            return {}
+            return {
+                //表头配置
+                columnData : salesUserHead,
+                //表格数据
+                tableData : [{}],
+                //总条数
+                totalCount : 0,
+            }
         },
         computed : {},
         created () {
@@ -23,10 +66,33 @@
         mounted () {
         },
         watch : {},
-        methods : {}
+        methods : {
+            /**
+             * 查询销售用户信息列表数据
+             */
+            queryList () {
+
+            },
+            /**
+             * 显示销售用户信息模态框
+             * @param scopeRow
+             */
+            showModal (scopeRow) {
+                this.$refs.infoDetailModal.show(scopeRow);
+            },
+        }
     };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    @import '~@/assets/scss/base';
 
+    .sales-user-info {
+        @include block_outline();
+        min-width: $content_min_width;
+        overflow: auto;
+        background: $color_fff;
+        border-radius: 4px;
+
+    }
 </style>
