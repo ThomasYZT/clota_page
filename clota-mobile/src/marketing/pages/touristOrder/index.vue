11def16f00ca068929499179e2aa3b9931d8256f
郑高红 <gaohong.zheng@workerbrother.com>
2018-12-06 17:20:42 +0800

    游客预定订单接口调试

--- clota-mobile/src/marketing/pages/touristOrder/index.vue
+++ clota-mobile/src/marketing/pages/touristOrder/index.vue
@@ -17,37 +17,16 @@
                 <template v-for="(item,index) in productList">
                     <product-info :product-info="item"
                                   :key="index"
+                                  :active-date="activeDate"
                                   @show-notice="showProductNoticeMethod">
                     </product-info>
                 </template>
             </scroll-wrap>
         </div>
         <!--购票须知模态框-->
-        <div v-transfer-dom>
-            <popup v-model="showProductNotice">
-                <div :class="$style.popupBody">
-                    <div :class="$style.head">
-                        <span :class="$style.known" @click="showProductNotice = false">{{$t('知道了')}}</span>
-                        购票须知
-                    </div>
-                    <div :class="$style.content">
-                        <div :class="$style.productTitle">广州长隆水上乐园成人单人票</div>
-                        <div :class="$style.productNotice">{{$t('colonSetting',{ key : $t('产品说明') })}}</div>
-                        <div :class="$style.notice">本产品包含：水上乐园全天双人票1张，野生动物园门票1张以及飞鸟乐园10选3门票1张。
-                            其中飞鸟乐园可选游玩项目包括：丛林飞龙、旋转木马、哪吒闹海。。。
-                            使用规则：两人同时检票入园，每张票限免费携带一名1.0米（不含）以下儿童入园。
-                            有效期：游玩日期当天起3天有效。
-                            适用条件：成人或身高1.5米（含）以上儿童。</div>
-                        <div :class="$style.productNotice">{{$t('colonSetting',{ key : $t('购票须知') })}}</div>
-                        <div :class="$style.notice">本产品包含：水上乐园全天双人票1张，野生动物园门票1张以及飞鸟乐园10选3门票1张。
-                            其中飞鸟乐园可选游玩项目包括：丛林飞龙、旋转木马、哪吒闹海。。。
-                            使用规则：两人同时检票入园，每张票限免费携带一名1.0米（不含）以下儿童入园。
-                            有效期：游玩日期当天起3天有效。
-                            适用条件：成人或身高1.5米（含）以上儿童。</div>
-                    </div>
-                </div>
-            </popup>
-        </div>
+        <ticket-notice :choosedProductInfo="choosedProductInfo"
+                       v-model="showProductNotice">
+        </ticket-notice>
         <!--日期选择模态框-->
         <full-date-modal v-model="chooseDateShow"
                          :active-date="activeDate"
@@ -63,6 +42,7 @@
     import fullDateModal from './components/fullDateModal';
     import ajax from '@/marketing/api/index';
     import { mapGetters } from 'vuex';
+    import ticketNotice from './components/ticketNotice';
 
 	export default {
 		data () {
@@ -79,22 +59,26 @@
                 //页码
                 pageNo : 1,
                 //每页条数
-                pageSize : 10
+                pageSize : 10,
+                //选择的产品信息
+                choosedProductInfo : {}
             };
 		},
         components : {
             dateHead,
             scrollWrap,
             productInfo,
-            fullDateModal
+            fullDateModal,
+            ticketNotice
         },
 		methods : {
             /**
              * 显示购票须知详情
-             * @param data
+             * @param{Object} data 订单信息
              */
             showProductNoticeMethod (data) {
                 this.showProductNotice = true;
+                this.choosedProductInfo = data;
             },
             /**
              * 查询游客订单信息
@@ -169,58 +153,5 @@
         .product-list{
             @include block_outline($height : unquote('calc(100% - 83px)'));
         }
-    }
-</style>
-<style module>
-    .popupBody{
-        width: 100%;
-        height: 400px;
-        background: #fff;
-    }
-    .head{
-        width: 100%;
-        height: 44px;
-        background: #F5F6F8;
-        position: relative;
-        text-align: center;
-        line-height: 44px;
-        font-size: 15px;
-        color: #666666;
-    }
-    .known{
-        position: absolute;
-        right: 10px;
-        font-size: 15px;
-        color: #046FDB;
-    }
-    .content{
-        height: 364px;
-        overflow: auto;
-        padding: 0 24px;
-    }
-    .productNotice{
-        height: 30px;
-        font-size: 15px;
-        color: #333333;
-        padding-bottom: 9px;
-        margin-top: 14px;
-    }
-    .notice{
-        font-size: 12px;
-        color: #999999;
-    }
-    .footer{
-        height: 38px;
-        background: #F5F6F8;
-        line-height: 38px;
-        text-align: right;
-    }
-    .price{
-        color: #F7981C;
-    }
-    .commission{
-        color: #046FDB;
-        margin-left: 20px;
-        padding-right: 10px;
     }
 </style>
