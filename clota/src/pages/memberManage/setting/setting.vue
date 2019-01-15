ad7af203f69ac8b7a62066142d29f2ff1b71fd80
yangzetao <624524240@qq.com>
2019-01-15 11:02:20 +0800

    充值通知模版id显示控制修改

--- clota/src/pages/memberManage/setting/setting.vue
+++ clota/src/pages/memberManage/setting/setting.vue
@@ -224,7 +224,7 @@
                             </div>
 
                             <div class="text" :class="{'ivu-form-item-error': error.chargeTemplateIdErr}"
-                                 v-if="memberConfigInfo.memberPoint === 'false'">
+                                 v-if="memberConfigInfo.memberRecharge === 'true'">
                                 <span class="text">{{$t('会员充值通知模版ID：')}}</span>
                                 <Input type="text"
                                        :disabled="!settingData.wxMpTemplateInfoSet.showStoreValue"
