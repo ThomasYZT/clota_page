6c2daa3ed84eb49c2074886b31a6d46e3f468d28
yangzetao <624524240@qq.com>
2019-02-14 15:44:02 +0800

    模态框样式修改

--- clota/src/pages/productCenter/components/tipModal.vue
+++ clota/src/pages/productCenter/components/tipModal.vue
@@ -4,14 +4,16 @@
 -->
 <template>
     <Modal v-model="visible"
+           class-name="vertical-center-modal"
+           width="400"
            :mask-closable="false">
         <div class="header" slot="header">
             <span>{{$t('notice')}}</span>
         </div>
 
-        <div class="remove-group">
-            <span class="red-bale">
-                <Icon type="help-circled"></Icon>
+        <div :class="$style['remove-group']">
+            <span :class="$style['red-bale']">
+                <Icon :class="$style.icon" type="help-circled"></Icon>
                 <span style="color:#EB6751;">{{$t('haveSomeUnsavedData')}}？</span>
             </span>
         </div>
@@ -55,5 +57,35 @@
             font-size: $font_size_16px;
             color: #354052;
         }
+    }
+
+    /deep/ .ivu-modal-body {
+        position: relative;
+        min-height: 168px;
+    }
+</style>
+<style module lang="scss">
+    .remove-group{
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        padding: 0 76px 0 106px;
+        color: #333333;
+        font-size: 14px;
+    }
+
+    .icon{
+        font-size: 15px;
+        color: #EB6751;
+    }
+
+    .red-bale {
+        color: #ED3F14;
     }
 </style>
