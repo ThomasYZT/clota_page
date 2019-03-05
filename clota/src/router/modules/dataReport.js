e4d3b765bcbe3eb3425f5893757a0ca649ec2fe1
郑高红 <gaohong.zheng@workerbrother.com>
2019-03-05 14:38:13 +0800

    注释修改

--- clota/src/router/modules/dataReport.js
+++ clota/src/router/modules/dataReport.js
@@ -2,13 +2,12 @@
  * 数据报表菜单路由，及组织架构子路由，这里是静态路由，不直接添加到$router中，
  * meta信息解释
  *  _name用来判断路由的权限
- * menuName 对应的语言配置文件当中的键值
+ * menuName 对应的语言配置文件当中的键值,如果路由有name属性，那么路由的name属性必须和menuName相同
  * iconClass 图标类名
  * rightPath 菜单点亮路径,格式为 menuName.menuName 第一个menuName表示一级点亮的菜单，第二个menuName表示当前点亮的左侧菜单（不管层级有多深）
  * isMenu 是否是菜单
  * isStaticMenu 表示不需要权限的菜单，如果配置了这个属性，可以不用再配置是_name
  */
-//一级菜单的menuname
 //一级菜单的menuname
 let topMenuName = 'dataReport';
 export default {
